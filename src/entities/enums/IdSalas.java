ebd4e4d1dc8f032107c7c058b61f7f2df4a80860
beltraoo <ericbeltraov@gmail.com>
2026-09-12 18:10:33 -0300

    feat: Refeito a estrutura do enum responsável por armazenar as salas da unidade, agora eles possuem um nome de exibição alteravel. A responsabilidade de exibir as informações virou do enum.

--- src/entities/enums/IdSalas.java
+++ src/entities/enums/IdSalas.java
@@ -1,14 +1,25 @@
 package entities.enums;
 
 public enum IdSalas {
-    SALA_01,
-    SALA_02,
-    SALA_03,
-    SALA_04,
-    SALA_05,
-    SALA_06,
-    SALA_07,
-    SALA_08,
-    SALA_09,
-    SALA_10
+    SALA_01("Sala 1"),
+    SALA_02("Sala 2"),
+    SALA_03("Sala 3"),
+    SALA_04("Sala 4"),
+    SALA_05("Sala 5"),
+    SALA_06("Sala 6"),
+    SALA_07("Sala 7"),
+    SALA_08("Sala 8"),
+    SALA_09("Sala 9"),
+    SALA_10("Sala 10");
+
+    private final String nomeExibicao;
+
+    IdSalas(String nomeExibicao) {
+        this.nomeExibicao = nomeExibicao;
+    }
+
+    @Override
+    public String toString() {
+        return nomeExibicao;
+    }
 }
