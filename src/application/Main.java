4c216ff56b781ab6252d25dc1b55707cb7d83282
beltraoo <ericbeltraov@gmail.com>
2026-09-12 18:11:03 -0300

    feat: Refeito toda a estrutura para realizar o agendamento.

--- src/application/Main.java
+++ src/application/Main.java
@@ -2,6 +2,7 @@ package application;
 
 import entities.Agendamento;
 import entities.enums.IdSalas;
+import entities.enums.ProgramacaoFuncionamento;
 
 import java.util.*;
 
@@ -11,6 +12,7 @@ public class Main {
         List<Agendamento> agendamentos = new ArrayList<>();
         Scanner input = new Scanner(System.in);
         Agendamento agendamento = null;
+        IdSalas idSalas = null;
 
         boolean repetir = true;
 
@@ -30,11 +32,50 @@ public class Main {
                     int numeroIdAgendamento = random.nextInt(5000);
                     System.out.println("== ID AGENDAMENTO: "+numeroIdAgendamento+" ==");
                     System.out.println("== Salas da Unidade: ");
-                    System.out.println(Arrays.toString(IdSalas.values()));
-                    System.out.println("Digite uma das salas acima: ");
-                    IdSalas escolhaSala = IdSalas.valueOf(input.nextLine());
-                    System.out.println("");
-                    agendamentos.add(agendamento = new Agendamento(numeroIdAgendamento,escolhaSala,));
+                    for(IdSalas sala : IdSalas.values()) {
+                        System.out.print(sala+" | ");
+                    }
+                    System.out.println();
+
+                    System.out.println("Digite a numeração de uma das salas acima: ");
+                    int numeracaoSala = input.nextInt();
+                    input.nextLine();
+
+                    switch (numeracaoSala) {
+                        case 1 -> idSalas = IdSalas.SALA_01;
+                        case 2 -> idSalas = IdSalas.SALA_02;
+                        case 3 -> idSalas = IdSalas.SALA_03;
+                        case 4 -> idSalas = IdSalas.SALA_04;
+                        case 5 -> idSalas = IdSalas.SALA_05;
+                        case 6 -> idSalas = IdSalas.SALA_06;
+                        case 7 -> idSalas = IdSalas.SALA_07;
+                        case 8 -> idSalas = IdSalas.SALA_08;
+                        case 9 -> idSalas = IdSalas.SALA_09;
+                        case 10 -> idSalas = IdSalas.SALA_10;
+                        default -> System.out.println("ERRO: Sala inválida, verifique a lista e escolha uma opção.");
+                    }
+
+                    System.out.println("== Dias e Horários: ");
+                    System.out.printf("%s, Abertura: %s - Fechamento: %s%n",ProgramacaoFuncionamento.SEGUNDA,ProgramacaoFuncionamento.SEGUNDA.getHoraAbertura(),ProgramacaoFuncionamento.SEGUNDA.getHoraFechamento());
+                    System.out.printf("%s, Abertura: %s - Fechamento: %s%n",ProgramacaoFuncionamento.TERCA,ProgramacaoFuncionamento.TERCA.getHoraAbertura(),ProgramacaoFuncionamento.TERCA.getHoraFechamento());
+                    System.out.printf("%s, Abertura: %s - Fechamento: %s%n",ProgramacaoFuncionamento.QUARTA,ProgramacaoFuncionamento.QUARTA.getHoraAbertura(),ProgramacaoFuncionamento.QUARTA.getHoraFechamento());
+                    System.out.printf("%s, Abertura: %s - Fechamento: %s%n",ProgramacaoFuncionamento.QUINTA,ProgramacaoFuncionamento.QUINTA.getHoraAbertura(),ProgramacaoFuncionamento.QUINTA.getHoraFechamento());
+                    System.out.printf("%s, Abertura: %s - Fechamento: %s%n",ProgramacaoFuncionamento.SEXTA,ProgramacaoFuncionamento.SEXTA.getHoraAbertura(),ProgramacaoFuncionamento.SEXTA.getHoraFechamento());
+                    System.out.println("Digite o dia para o agendamento: ");
+                    String diaFuncionamento = input.nextLine();
+
+                    ProgramacaoFuncionamento programacaoFuncionamento = null;
+
+                    switch (diaFuncionamento.toUpperCase()) {
+                        case "SEGUNDA" -> programacaoFuncionamento = ProgramacaoFuncionamento.SEGUNDA;
+                        case "TERCA" -> programacaoFuncionamento = ProgramacaoFuncionamento.TERCA;
+                        case "QUARTA" -> programacaoFuncionamento = ProgramacaoFuncionamento.QUARTA;
+                        case "QUINTA" -> programacaoFuncionamento = ProgramacaoFuncionamento.QUINTA;
+                        case "SEXTA" -> programacaoFuncionamento = ProgramacaoFuncionamento.SEXTA;
+                        default -> System.out.println("ERRO: Dia inválido, verifique a lista e escolha uma opção.");
+                    }
+//                    System.out.println("");
+//                    agendamentos.add(agendamento = new Agendamento(numeroIdAgendamento,escolhaSala,));
                     break;
             }
 
