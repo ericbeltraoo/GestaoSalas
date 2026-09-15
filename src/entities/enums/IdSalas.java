e506cd243248a22a142c87c34863725293ab4c81
ericbeltraoo <ericbeltraov@gmail.com>
2026-09-15 16:01:40 -0300

    feat: Alterado os nomes das salas

--- src/entities/enums/IdSalas.java
+++ src/entities/enums/IdSalas.java
@@ -1,14 +1,14 @@
 package entities.enums;
 
 public enum IdSalas {
-    SALA_01("Sala 1"),
-    SALA_02("Sala 2"),
-    SALA_03("Sala 3"),
-    SALA_04("Sala 4"),
-    SALA_05("Sala 5"),
-    SALA_06("Sala 6"),
-    SALA_07("Sala 7"),
-    SALA_08("Sala 8"),
+    SALA_01("Sala 1 - Auditório"),
+    SALA_02("Sala 2 - Auditório"),
+    SALA_03("Sala 3 - Sala de Reunião"),
+    SALA_04("Sala 4 - Sala de Reunião"),
+    SALA_05("Sala 5 - Sala de Estudos"),
+    SALA_06("Sala 6 - Sala de Descanço"),
+    SALA_07("Sala 7 - Sala Executiva"),
+    SALA_08("Sala 8 - Sala Executiva"),
     SALA_09("Sala 9"),
     SALA_10("Sala 10");
 
