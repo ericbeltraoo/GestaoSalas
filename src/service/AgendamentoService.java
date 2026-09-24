3b816adab73f6955c71eaa1659da8b1127b10eb1
beltraoo <ericbeltraov@gmail.com>
2026-09-24 00:14:02 -0300

    feat: Criado a função que verifica se os horários das salas estão entrando em conflito

--- src/service/AgendamentoService.java
+++ src/service/AgendamentoService.java
@@ -10,11 +10,23 @@ public class AgendamentoService {
     public static final int TEMPO_MINIMO_RESERVA = 30;
 
     //verificação se um agendamento entra em conflito com outro
-//    public boolean possuiConflito(List<Agendamento> agendamentos) {
-//        for(Agendamento x : agendamentos) {
-//            x.getSalaId()
-//        }
-//    }
+    //antigo = a , novo = b;
+    public boolean possuiConflito(List<Agendamento> agendamentos, Agendamento novo) {
+        boolean verificacao = false;
+        for(Agendamento existentes : agendamentos) {
+            if(existentes.getSalaId() == novo.getSalaId()) {
+                 if(existentes.getInicio().isBefore(novo.getFim()) && novo.getInicio().isBefore(existentes.getFim())) {
+                   verificacao = true;
+                 } else {
+                    verificacao = false;
+                 }
+             } else {
+                // a sala nao é igual, entao nao tem como o horario ser conflitado.
+               verificacao = false;
+            }
+        }
+      return verificacao;
+    }
 
     public long calcularHorasTotaisFormatadas(List<Agendamento> agendamentos, IdSalas idSalas) {
         Duration duracao = null;
